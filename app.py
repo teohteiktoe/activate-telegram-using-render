06b7e947d2cbf7b8c4e84be0025c899bcd2a8deb
Teoh TeikToe <teohteiktoe@gmail.com>
2025-11-22 01:57:54 +0000

    1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import time
 import requests
 
-TOKEN = '77932853004:AAFw8FC_DTNi9n0RlsBuzP6DrNYSC8O7DJo'
+TOKEN = '8250779616:AAHSKtI8Pe-ns4L8hj68JDU62CnNIAL8et4'
 BASE_URL = f'https://api.telegram.org/bot{TOKEN}/'
 
 app = Flask(__name__)
@@ -14,28 +14,30 @@ def index():
 @app.route('/telegram', methods=['GET','POST'])
 def telegram():
     #grab id
-    time.sleep(5)
+    time.sleep(10)
+    print("loop")
     response = requests.get(BASE_URL + 'getUpdates')
     data = response.json()
     text = data['result'][-1]['message']['text']
     chat_id = data['result'][-1]['message']['chat']['id']
     print("Text:", text)
     print("Chat ID:", chat_id)
-    send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction, please enter the salary"}'
+    send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction Food expenditure, please enter the salary"}'
     requests.get(send_url)
-    time.sleep(3)
+    time.sleep(10)
     response = requests.get(BASE_URL + 'getUpdates')
     data = response.json()
     text = data['result'][-1]['message']['text']
     if text.isnumeric():
-        msg = str(float(text) * 100 + 10)
+        msg = str(float(text) * 0.1)
+        msg = "Your predicted expenditure is " + msg
         send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={msg}'
         requests.get(send_url)
     else:
         msg = "salary must be a number"
         send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={msg}'
         requests.get(send_url)
-        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction, please enter the salary"}'
+        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction Food expenditure, please enter the salary"}'
         requests.get(send_url)
         time.sleep(3)
     return(render_template("index.html"))
